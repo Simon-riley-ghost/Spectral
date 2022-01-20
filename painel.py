3434cd6b9c0a4f94acddd714944b3a3fae2c32f4
@VanGogh <91226605+Simon-riley-ghost@users.noreply.github.com>
2022-01-20 14:14:40 -0300

    Add files via upload

---
+++ painel.py
@@ -0,0 +1,426 @@
+# biblioteca usadas
+from asyncio import windows_events
+from turtle import clear
+import data,colorama,requests,time,os
+colorama.init()
+######################
+#                    #
+#   Area de animação #
+#   Extenso          #
+#                    #
+######################
+def animacao_banner():
+    time.sleep(1)
+    if windows_events:
+        os.system('cls')
+    elif clear:
+        os.system('clear')
+    print(
+        '''\033[1;31m
+               ...                                                 s                                   .. 
+   .x888888hx    :                                        :8                             x .d88"  
+  d88888888888hxx   .d``                                 .88       .u    .                5888R   
+ 8" ... `"*8888%`   @8Ne.   .u        .u          .     :888ooo  .d88B :@8c        u      '888R   
+!  "   ` .xnxx.     %8888:u@88N    ud8888.   .udR88N  -*8888888 ="8888f8888r    us888u.    888R   
+X X   .H8888888%:    `888I  888. :888'8888. <888'888k   8888      4888>'88"  .@88 "8888"   888R   
+X 'hn8888888*"   >    888I  888I d888 '88%" 9888 'Y"    8888      4888> '    9888  9888    888R   
+X: `*88888%`     !    888I  888I 8888.+"    9888        8888      4888>      9888  9888    888R   
+'8h.. ``     ..x8>  uW888L  888' 8888L      9888       .8888Lu=  .d888L .+   9888  9888    888R   
+ `88888888888888f  '*88888Nu88P  '8888c. .+ ?8888u../  ^%888*    ^"8888*"    9888  9888   .888B . 
+  '%8888888888*"   ~ '88888F`     "88888%    "8888P'     'Y"        "Y"      "888*""888"  ^*888%  
+     ^"****""`        888 ^         "YP'       "P'                            ^Y"   ^Y'     "%    
+                      *8E                                                                         
+                      '8>                                                                         
+                       "                                                                          
+        '''
+    )
+    time.sleep(1)
+    if windows_events:
+        os.system('cls')
+    elif clear:
+        os.system('clear')
+    print(
+        '''\033[1;32m
+  ██████  ██▓███  ▓█████  ▄████▄  ▄▄▄█████▓ ██▀███   ▄▄▄       ██▓    
+▒██    ▒ ▓██░  ██▒▓█   ▀ ▒██▀ ▀█  ▓  ██▒ ▓▒▓██ ▒ ██▒▒████▄    ▓██▒    
+░ ▓██▄   ▓██░ ██▓▒▒███   ▒▓█    ▄ ▒ ▓██░ ▒░▓██ ░▄█ ▒▒██  ▀█▄  ▒██░    
+  ▒   ██▒▒██▄█▓▒ ▒▒▓█  ▄ ▒▓▓▄ ▄██▒░ ▓██▓ ░ ▒██▀▀█▄  ░██▄▄▄▄██ ▒██░    
+▒██████▒▒▒██▒ ░  ░░▒████▒▒ ▓███▀ ░  ▒██▒ ░ ░██▓ ▒██▒ ▓█   ▓██▒░██████▒
+▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░░ ▒░ ░░ ░▒ ▒  ░  ▒ ░░   ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░▓  ░
+░ ░▒  ░ ░░▒ ░      ░ ░  ░  ░  ▒       ░      ░▒ ░ ▒░  ▒   ▒▒ ░░ ░ ▒  ░
+░  ░  ░  ░░          ░   ░          ░        ░░   ░   ░   ▒     ░ ░   
+      ░              ░  ░░ ░                  ░           ░  ░    ░  ░
+                         ░                                            
+        '''
+    )
+    time.sleep(1)
+    if windows_events:
+        os.system('cls')
+    elif clear:
+        os.system('clear')
+    print(
+        '''\033[1;33m
+ .oooooo..o                                    .                      oooo  
+d8P'    `Y8                                  .o8                      `888  
+Y88bo.      oo.ooooo.   .ooooo.   .ooooo.  .o888oo oooo d8b  .oooo.    888  
+ `"Y8888o.   888' `88b d88' `88b d88' `"Y8   888   `888""8P `P  )88b   888  
+     `"Y88b  888   888 888ooo888 888         888    888      .oP"888   888  
+oo     .d8P  888   888 888    .o 888   .o8   888 .  888     d8(  888   888  
+8""88888P'   888bod8P' `Y8bod8P' `Y8bod8P'   "888" d888b    `Y888""8o o888o 
+             888                                                            
+            o888o                                                           
+                                                                            
+        '''
+    )
+    time.sleep(1)
+    if windows_events:
+        os.system('cls')
+    elif clear:
+        os.system('clear')
+    print(
+        '''\033[1;34m
+    .dMMMb  dMMMMb  dMMMMMP .aMMMb dMMMMMMP dMMMMb  .aMMMb  dMP 
+  dMP" VP dMP.dMP dMP     dMP"VMP   dMP   dMP.dMP dMP"dMP dMP  
+  VMMMb  dMMMMP" dMMMP   dMP       dMP   dMMMMK" dMMMMMP dMP   
+dP .dMP dMP     dMP     dMP.aMP   dMP   dMP"AMF dMP dMP dMP    
+VMMMP" dMP     dMMMMMP  VMMMP"   dMP   dMP dMP dMP dMP dMMMMMP 
+                                                               
+        '''
+    )
+    time.sleep(1)
+    if windows_events:
+        os.system('cls')
+    elif clear:
+        os.system('clear')
+    print(
+        '''\033[1;35m
+                                                                                             
+      #######                                                                ###     
+    /       ###                                                               ###    
+   /         ##                                    #                           ##    
+   ##        #                                    ##                           ##    
+    ###                                           ##                           ##    
+   ## ###           /###     /##       /###     ######## ###  /###     /###    ##    
+    ### ###        / ###  / / ###     / ###  / ########   ###/ #### / / ###  / ##    
+      ### ###     /   ###/ /   ###   /   ###/     ##       ##   ###/ /   ###/  ##    
+        ### /##  ##    ## ##    ### ##            ##       ##       ##    ##   ##    
+          #/ /## ##    ## ########  ##            ##       ##       ##    ##   ##    
+           #/ ## ##    ## #######   ##            ##       ##       ##    ##   ##    
+            # /  ##    ## ##        ##            ##       ##       ##    ##   ##    
+  /##        /   ##    ## ####    / ###     /     ##       ##       ##    /#   ##    
+ /  ########/    #######   ######/   ######/      ##       ###       ####/ ##  ### / 
+/     #####      ######     #####     #####        ##       ###       ###   ##  ##/  
+|                ##                                                                  
+ \)              ##                                                                  
+                 ##                                                                  
+                 ##                                                                 
+        '''
+    )
+    time.sleep(1)
+    if windows_events:
+        os.system('cls')
+    elif clear:
+        os.system('clear')
+    print(
+        '''\033[1;31m
+:'######::'########::'########::'######::'########:'########:::::'###::::'##:::::::
+'##... ##: ##.... ##: ##.....::'##... ##:... ##..:: ##.... ##:::'## ##::: ##:::::::
+ ##:::..:: ##:::: ##: ##::::::: ##:::..::::: ##:::: ##:::: ##::'##:. ##:: ##:::::::
+. ######:: ########:: ######::: ##:::::::::: ##:::: ########::'##:::. ##: ##:::::::
+:..... ##: ##.....::: ##...:::: ##:::::::::: ##:::: ##.. ##::: #########: ##:::::::
+'##::: ##: ##:::::::: ##::::::: ##::: ##:::: ##:::: ##::. ##:: ##.... ##: ##:::::::
+. ######:: ##:::::::: ########:. ######::::: ##:::: ##:::. ##: ##:::: ##: ########:
+:......:::..:::::::::........:::......::::::..:::::..:::::..::..:::::..::........::
+        '''
+    )
+    time.sleep(1)
+    if windows_events:
+        os.system('cls')
+    elif clear:
+        os.system('clear')
+
+
+# Cor CMD 
+os.system('color 02')
+
+#   Cores 
+Vermelho = '\033[1;31m'; fm = '\033[1;41m'
+Verde    =  '\033[1;32m'; fv = '\033[1;42m'
+Amarelo  = '\033[1;33m'; fa = '\033[1;43m'
+Azul 	 = '\033[1;34m'; faz = '\033[1;44m'
+Magenta  = '\033[1;35m'; fmag = '\033[1;45m' 
+
+# About 
+print(
+    '''
+    ------------------------------------------------------------------------------------
+    Bem isso e um painel feito para a Spectral 
+    bem eu criei ele sem patrocinio 
+    então dei o meu melhor foram 3 dias para juntar a APIs e 2 dias para criar as defs e etc 
+    ele e totalmente gratuito e esta disponivel na GitHun
+    -------------------------------------------------------------------------------------
+    planos para ele:
+    1. caso a Spectral acabe ele sera deletado e o codigo saira da GitHub
+    2. ele nao possui banco de dados cada usuario e senha foi criado a mao 
+    3. ele podera receber atualização com o tempo mais nao a nada confirmado sobre isso 
+    -------------------------------------------------------------------------------------
+    para ele nao dar erro instale todas as bibliotecas dando o comando 
+    pip install -r requirements.txt
+    -------------------------------------------------------------------------------------
+    '''
+)
+
+# Confirmação de usuario
+login = 1
+if login == 1:
+    print('Logo abaixo ira pedir seu nick na Spectral')
+    print(' ')
+
+    time.sleep(2)
+
+    user = input('Nick : ') # pra insirir seu nome 
+    if input('User: ') .strip() == user:
+        print('\033[1;31mBuascando dados...') # coloquei apenas por stazk
+
+    else: # caso não exista usuario ele ira dizer espera um time e fecha o progrma
+        print('usuario não existe')
+        time.sleep(3)
+        os.system('cls')
+        exit()
+
+    time.sleep(1)
+    print('\033[1;32mdados encontrados.')
+    print ('\033[1;31minsira a senha para confirma identidade') # ele ira te pedir a senha e a senha tem que ser igual
+
+    snh = 'adm' # Senha 
+    if input('Pass: ') .strip() == snh: #vai confirma se a senha e valida
+     print ('''\033[1;31m
+    informação de login:
+    Nome : {}
+    Codenome : N/A
+    '''.format(user))
+
+    else:  # caso a senha não seja ele irar fechar o script
+        print ('\033[1;31mOps você não tem autorização para entrar no painel')
+        exit()
+
+# Banner
+animacao_banner()
+
+#######################################################
+# AREA IMPORTANTE NAO MEXA SE NN POSSUIR CONHECIMENTO #
+#######################################################
+#######################################
+#             AREA DE APIs            #
+#######################################
+def cep():
+    import requests
+
+    print('\033[1;34m####################')
+    print('\033[1;34m### consulta CEP ###')
+    print('\033[1;34m####################')
+    print()
+    cep_input = input('Digite o CEP : ')
+    
+    if len(cep_input) != 8:
+        print('quantidade de digito errada')
+        exit()
+
+    request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep_input))
+
+    address_data = request.json()
+
+    if 'erro' not in address_data:
+        print('\033[1;34m--> cep encontrado <--')
+
+        print('\033[1;31m[> CEP <] : {}'.format(address_data['cep']))
+        print('\033[1;31m[> Logradouro <] : {}'.format(address_data['logradouro']))
+        print('\033[1;31m[> Complemento <] : {}'.format(address_data['complemento']))
+        print('\033[1;31m[> Bairro <] : {}'.format(address_data['bairro']))
+        print('\033[1;31m[> Localidade <] : {}'.format(address_data['localidade']))
+        print('\033[1;31m[> U.F <] : {}'.format(address_data['uf']))
+        print('\033[1;31m[> DDD <] : {}'.format(address_data['ddd']))
+
+    else:
+        print('{} : CEP invalido'.format(cep_input))
+def consulta_nome():
+    import requests
+    print('#####################################################')
+    print('### Busca Nome ### coloque apenas o primeiro nome ###')
+    print('#####################################################')
+    nome_input = input('Nome da pessoa : ')
+
+    request_nome = requests.get('https://servicodados.ibge.gov.br/api/v2/censos/nomes/{}'.format(nome_input))
+    nome_data = request_nome.json()
+    if 'erro' not in nome_data:
+        print('\033[1;34m--> nome encontrado <--')
+        print('----------------------------------------------------------------------------')
+        print('Resultado 1 : {}'.format(nome_data[0,1,2])) 
+        #print('Nome : {}'.format(nome_data['nome']))
+        #print('Sexo : {}'.format(nome_data['sexo']))
+        #print('Localidade : {}'.format(nome_data['localidade']))
+
+    print('----------------------------------------------------------------------------')
+def cnpj():
+    import requests
+    cpnj_input = input('Digite o cnpj :  ')
+    
+    if len(cpnj_input) != 14:
+        print('quantidade de digito errada')
+        exit()
+
+    r_cnpj = requests.get('https://www.receitaws.com.br/v1/cnpj/{}'.format(cpnj_input))
+    data_cp = r_cnpj.json()
+    if 'erro' not in data_cp:
+        print('')
+        print('--> Atividade Principal <--')
+        print(' ')
+        print('data_situacao: {}'.format(data_cp['data_situacao']))
+        print('complemento: {}'.format(data_cp['complemento']))
+        print('tipo: {}'.format(data_cp['tipo']))
+        print('nome:{}'.format(data_cp['nome']))
+        print('uf: {}'.format(data_cp['uf']))
+        print('telefone:  {}'.format(data_cp['telefone']))
+        print('email: {}'.format(data_cp['email']))
+        print(' ')
+def placa():
+    import requests
+    print('----------------------')
+    print('    Consultar Placa   ')
+    print('----------------------')
+    
+    placa_input = input('Digite a placa : ')
+    print(' ')
+    if len(placa_input) != 7:
+        print('quantidade de digito errada')
+        exit()
+    request = requests.get('https://apicarros.com/v1/consulta/{}'.format(placa_input))
+    placa_data = request.json()
+    if 'erro' not in placa_data:
+        print('Placa Localizada')
+        print(' ')
+        print('Ano : {}'.format(placa_data['ano']))
+        print('Chassi : {}'.format(placa_data['chassi']))
+        print('Codigo Retorno : {}'.format(placa_data['codigoRetorno']))
+        print('Codigo Situacao : {}'.format(placa_data['codigoSituacao']))
+        print('Cor : {}'.format(placa_data['cor']))
+        print('Data : {}'.format(placa_data['data']))
+        print('Data Atualizacao Alarme : {}'.format(placa_data['dataAtualizacaoAlarme']))
+        print('Data Atualizacao Roubo Furto : {}'.format(placa_data['dataAtualizacaoRouboFurto']))
+        print('Marca : {}'.format(placa_data['marca']))
+        print('Mensagem Retorno : {}'.format(placa_data['mensagemRetorno']))
+        print('Modelo : {}'.format(placa_data['modelo']))
+        print('Municipio : {}'.format(placa_data['municipio']))
+        print('Placa : {}'.format(placa_data['placa']))
+        print('Situacao : {}'.format(placa_data['situacao']))
+        print('U.F : {}'.format(placa_data['uf']))
+def ip():
+    import requests
+    ip_input = input('Digite o ip : ')
+    print(' ')
+    if len(ip_input) != 8:
+        print('quantidade de digito errada')
+        exit()
+    request = requests.get('http://ipwhois.app/json/{}'.format(ip_input))
+    ip_data = request.json()
+    if 'erro' not in ip_data:
+        print('--> Resultados Full <--')
+        print(' ')
+        print('Ip : {}'.format(ip_data['ip']))
+        print('Success : {}'.format(ip_data['success']))
+        print('Tipo : {}'.format(ip_data['type']))
+        print('continente : {}'.format(ip_data['continent']))
+        print('código continente : {}'.format(ip_data['continent_code']))
+        print('país : {}'.format(ip_data['country']))
+        print('Código do país : {}'.format(ip_data['country_code']))
+        print('bandeira do país : {}'.format(ip_data['country_flag']))
+        print('país capital : {}'.format(ip_data['country_capital']))
+        print('país telefone : {}'.format(ip_data['country_phone']))
+        print('vizinhos do país : {}'.format(ip_data['country_neighbours']))
+        print('região : {}'.format(ip_data['region']))
+        print('cidade : {}'.format(ip_data['city']))
+        print('Latitude : {}'.format(ip_data['latitude']))
+        print('Longitude : {}'.format(ip_data['longitude']))
+        print('Asn : {}'.format(ip_data['asn']))
+        print('Org : {}'.format(ip_data['org']))
+        print('Isp : {}'.format(ip_data['isp']))
+        print('fuso horário : {}'.format(ip_data['timezone']))
+        print('nome do fuso horário : {}'.format(ip_data['timezone_name']))
+        print(' : {}'.format(ip_data['completed_requests']))
+def covid():
+    import requests
+    covid_input = input('Digite a U.F : ')
+    print(' ')
+    if len(covid_input) != 2:
+        print('quantidade de digito errada')
+        exit()
+    request = requests.get('https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/{}'.format(covid_input))
+    covid_data = request.json()
+    if 'erro' not in covid_data:
+        print('--> Resultados <--')
+        print(' ')
+        print('UID : {}'.format(covid_data['uid']))
+        print('U.F : {}'.format(covid_data['uf']))
+        print('Estado : {}'.format(covid_data['state']))
+        print('Casos : {}'.format(covid_data['cases']))
+        print('Mortes : {}'.format(covid_data['deaths']))
+        print('Suspeitos : {}'.format(covid_data['suspects']))
+        print('Recusa : {}'.format(covid_data['refuses']))
+        print('Hora e Data : {}'.format(covid_data['datetime']))
+
+
+#            CODIGO MENU           #
+ans = True
+while ans:
+
+    print('Bem Vindo Senhor {}'.format(user))
+    print('O painel ta fresquinho hoje Senhor {}'.format(user))
+    print('')
+    print(f'{Verde}[CODE][ON][1] Consulta CEP')
+    print(f'{Vermelho}[CODE][OFF][2] Consultar Nome')
+    print(f'{Verde}[CODE][ON][3] Consultar CNPJ')
+    print(f'{Verde}[CODE][ON][4] Consultar Placa')
+    print(f'{Verde}[CODE][ON][5] Consultar Ip')
+    print(f'{Verde}[CODE][ON][6] Dados Covid')
+    print(f'{Verde}[LINK][ON][7] Spectral')
+    print(f'{Verde}[EXIT][ON][98] Sair')
+    print(f'{Verde}[FUNÇÃO][ON][99] Limpar')
+
+    dec = input('Escolha : ')
+
+    if '1' == dec:
+        cep()
+    elif '2' == dec:
+        print('esta funcionado incorretamente aguarde um atualização')
+        consulta_nome()
+    elif '3' == dec:
+        cnpj()
+    elif '4' == dec:
+        placa()
+    elif '5' == dec:
+        ip()
+    elif '6' == dec:
+        covid()
+    elif '7' == dec:
+        print('entre em contatoc com um dos adms\n')
+    elif '98' == dec:
+        exit()
+    elif '99' == dec:
+        if windows_events:
+            os.system('cls')
+        elif clear:
+            os.system('clear')
+
+    else:
+        print('Opção invalida.')
+        
+
+
+
+
+
+
+
+
+
+
